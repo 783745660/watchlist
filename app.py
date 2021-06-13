442f5017eacedbf9139bfaa3cdb9643d7dbaf25b
songwenwen <783745660@qq.com>
2021-06-13 19:40:46 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os, sys
 
 import click
-from flask import Flask, escape, render_template, url_for
+from flask import Flask, escape, redirect, render_template, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy 
 
 
@@ -15,6 +15,7 @@ else:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -89,9 +90,49 @@ def page_not_found(e):
     user = User.query.first()
     return render_template('404.html', user=user), 404
 
-@app.route('/')
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invaild input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = User.query.first()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
