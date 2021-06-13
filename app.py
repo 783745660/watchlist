bfe3110ab06786b7200ace94696fe3a5ad085880
Song wenwen <783745660@qq.com>
2021-06-13 13:51:37 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from flask import Flask, escape, url_for
+from flask import Flask, escape, render_template, url_for
+
+
 app = Flask(__name__)
+
+name = 'Song wenwen'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999',},
+    {'title': 'Devils on the Doorstep', 'yeaer': '1999'},
+    {'title': 'WALL-E', 'year':'2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/home')
 def hello():
@@ -16,6 +32,10 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2))
     return 'Test page'
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 if __name__ == "__main__":
