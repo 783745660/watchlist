f12265fd64c065b091430e593fe67e7d2dc99b15
songwenwen <783745660@qq.com>
2021-06-13 16:27:12 +0800

    Add static files

--- app.py
+++ app.py
@@ -79,11 +79,21 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    return render_template('404.html', user=user), 404
+
 @app.route('/')
 def index():
     user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 
 if __name__ == "__main__":
